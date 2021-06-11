2f1dbcc0795cbee38f43e626e73ab54d2146ccf6
Ciremun <Ciremun@yandex.ru>
2021-06-11 17:36:47 +0300

    notify user if id was not found

--- redirect.php
+++ redirect.php
@@ -7,5 +7,10 @@ include 'db.php';
 $_GET['url_id'] = pg_escape_string($_GET['url_id']);
 $redirect_url = pg_select($conn, 'urls', $_GET);
 
+if (empty($redirect_url)) {
+    echo("redirect url was not found for id {$_GET['url_id']}");
+    die();
+}
+
 header("Location: {$redirect_url[0]['stored_url']}");
 die();
