7d4cc907153d5cc0019b705a5ddf9ce43833588c
Ciremun <Ciremun@yandex.ru>
2021-06-11 17:36:19 +0300

    move shorten.html

--- shorten.php
+++ shorten.php
@@ -10,5 +10,17 @@ pg_close($conn);
 
 $shorten_url = htmlspecialchars("{$_SERVER['HTTP_HOST']}/{$_POST['url_id']}", ENT_QUOTES, 'UTF-8');
 $shorten_url =  '<a href="/' . $_POST['url_id'] . '">' . $shorten_url . '</a>';
+?>
 
-include 'shorten.html';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="reset.css">
+    <title>url-shortener - <?= $_POST['url_id'] ?></title>
+</head>
+<body>
+    <?= $shorten_url ?>
+</body>
+</html>
