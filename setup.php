86673fa977bdde34fd558091475ca4e1a6a44c40
Ciremun <Ciremun@yandex.ru>
2021-06-12 12:25:58 +0300

    use pg_query

--- setup.php
+++ setup.php
@@ -2,8 +2,8 @@
 
 include 'db.php';
 
-pg_exec($conn, 'CREATE TABLE IF NOT EXISTS urls (id SERIAL PRIMARY KEY, url_id text, stored_url text);');
-pg_exec($conn, 'CREATE TABLE IF NOT EXISTS server_state (id SERIAL PRIMARY KEY, k text, v text);');
+pg_query($conn, 'CREATE TABLE IF NOT EXISTS urls (id SERIAL PRIMARY KEY, url_id text, stored_url text);');
+pg_query($conn, 'CREATE TABLE IF NOT EXISTS server_state (id SERIAL PRIMARY KEY, k text, v text);');
 
 if (empty(get_server_state('url_id_length'))) {
     add_server_state('url_id_length', 2);
