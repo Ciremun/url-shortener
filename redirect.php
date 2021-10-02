f7b607905b689948d8394381ab21e7f3074ef4a3
Ciremun <Ciremun@yandex.ru>
2021-10-03 01:33:49 +0300

    htmlspecialchars in redirect

--- redirect.php
+++ redirect.php
@@ -7,7 +7,7 @@ include 'db.php';
 $redirect_url = pg_select($conn, 'urls', $_GET, PGSQL_DML_ESCAPE);
 
 if (empty($redirect_url)) {
-    echo ("redirect url was not found for id {$_GET['url_id']}");
+    echo("redirect url was not found for id " . htmlspecialchars($_GET['url_id'], ENT_QUOTES, 'UTF-8'));
     die();
 }
 
